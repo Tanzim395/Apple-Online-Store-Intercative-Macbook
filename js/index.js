551d5d67fd918cfa947efb1b2579f09850fe24bb
Tanzim395 <junaidulkader@gmail.com>
2021-08-21 23:14:37 +0600

    promo code fixed

--- js/index.js
+++ js/index.js
@@ -3,34 +3,29 @@ const buttons = document.getElementsByTagName("button");
 const bestPrice = document.getElementById("best-price");
 const memoryCost = document.getElementById("memory-cost");
 const storageCost = document.getElementById("storage-cost");
-const shippingCharge = document.getElementById("delivery-charge");
-const couponCode = document.getElementById("promo-code");
+const deliveryCharge = document.getElementById("delivery-charge");
+const promoCode = document.getElementById("promo-code");
 const sumPrice = document.getElementsByClassName("total-price");
 
-// Apply Promo Code to get 20% discount
-function executeCoupon() {
-    for (totalPrice of sumPrice) {
-        if (totalPrice.innerText < 0) {
-            totalPrice.innerText = 0;
-        };
-        //implementing 20% discount calc
-        const previousTotal = parseFloat(totalPrice.innerText);
-        const discount = (previousTotal * 20) / 100;
-        totalPrice.innerText = previousTotal - discount;
-        couponCode.value = "";
-    };
+// Apply Promo Code
+function applyPromoCode() {
+    const totalPrice = sumPrice[1];
+    const previousTotal = parseFloat(totalPrice.innerText);
+    const discount = (previousTotal * 20) / 100;
+    totalPrice.innerText = previousTotal - discount;
+    promoCode.value = "";
 };
 
-// Total Price Calculation
+// Calculate Total Price
 function calculateTotalPrice() {
-    const total = parseFloat(bestPrice.innerText) + parseFloat(memoryCost.innerText) + parseFloat(storageCost.innerText) + parseFloat(shippingCharge.innerText);
+    const total = parseFloat(bestPrice.innerText) + parseFloat(memoryCost.innerText) + parseFloat(storageCost.innerText) + parseFloat(deliveryCharge.innerText);
 
     for (totalPrice of sumPrice) {
         totalPrice.innerText = total;
     };
 };
 
-// Button Clicking
+// Button Click
 for (button of buttons) {
     button.addEventListener("click", function (event) {
         const clickedButton = event.target.innerText;
@@ -62,22 +57,24 @@ for (button of buttons) {
             calculateTotalPrice();
         }
 
-        // Delivery Charge
+        // Shipping Charge
         else if (clickedButton == "Friday, Aug 25 FREE Prime Delivery") {
-            shippingCharge.innerText = 0;
+            deliveryCharge.innerText = 0;
             calculateTotalPrice();
         }
 
         else if (clickedButton == "Friday, Aug 21 Delivery charge $20") {
-            shippingCharge.innerText = 20;
+            deliveryCharge.innerText = 20;
             calculateTotalPrice();
         }
 
-        // Promo Code "stevekaku"
+        // Promo code: Stevekaku
         else {
-            if (couponCode.value == "stevekaku") {
-                executeCoupon();
-            };
-        };
-    });
+            if (promoCode.value == "stevekaku") {
+                applyPromoCode();
+                //bttn disable
+                event.target.disabled = true;
+            }
+        }
+    })
 };
